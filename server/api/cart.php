630cba0b7bb32ddc0b566a86ccc6fec07ecd9469
Vandana Khaparde <vandana.khaparde@gmail.com>
2019-11-21 06:13:03 +0000

    Almost done with cart.php - Vandana

--- server/api/cart.php
+++ server/api/cart.php
@@ -1,28 +1,81 @@
 <?php
 
 if ($request['method'] === 'GET') {
+  $sessionId = $_SESSION['cart_id'];
+  print_r("$sessionId :", $sessionId);
   if (!isset($_SESSION['cart_id'])) {
     $response['body'] = [];
     send($response);
   }
+
+  $link = get_db_link();
+  // $message = check_connection($link,$sessionId);
+  $sql = "SELECT p.productId, p.name, p.image ,p.price, p.shortDescription, ci.cartItemId, c.cartId
+           FROM products AS p
+           JOIN  cartItems AS ci
+           ON  p.productId = ci.cartItemId
+           JOIN  carts AS c
+           ON ci.cartId  = c.cartId
+           WHERE c.cartId = $sessionId ";
+  $result = mysqli_query($link, $sql);
+  $message = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $response['body'] = $message;
+  send($response);
+}
 else if ($request['method'] === 'POST') {
     if (!isset($request['body']['productId']) || !is_numeric($request['body']['productId'])) {
       throw new ApiError("Valid productId is required", 400);
     }
-  }
-
     $link = get_db_link();
-    print_r($link);
-    $message = add_product_toCart($link);
-    $response['body'] = $message;
+    $productId = $request['body']['productId'];
+    $message = add_product_toCart($link,$productId);
+    $response['body'] = $message ['cart_items'];
+    $_SESSION['cart_id'] =  $message['cart_id'];
     send($response);
-    print("Session already started");
-
 }
 
-function add_product_toCart($link)
+// function check_connection($link, $sessionId){
+//   $sql = "SELECT p.productId, p.name, p.image ,p.price, p.shortDescription,ci.cartItemId,c.cartId
+//            FROM products AS p
+//           JOIN  cartItems AS ci
+//            ON  p.productId = ci.cartItemId
+//            JOIN  carts AS c
+//            ON ci.cartId  = c.cartId
+//            WHERE c.cartId = $sessionId ";
+//   $result = mysqli_query($link, $sql);
+//   return mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// }
+
+function add_product_toCart($link,$productId)
 {
-  $sql = 'SELECT * FROM carts';
-  $link->query($sql);
-  return 'Succesfully connected to mysql';
+  $query = "SELECT price FROM products WHERE productId=$productId";
+
+  $result = mysqli_query($link, $query);
+  $prod_price =  mysqli_fetch_assoc($result);
+  $price = $prod_price['price'];
+  // return $price;
+  $insertIntoCartQuery= "INSERT INTO carts (createdAt) VALUES (CURRENT_TIMESTAMP)";
+  $insertIntoCartResult = mysqli_query($link, $insertIntoCartQuery);
+  $insert_id_cart = $link->insert_id;
+  // return $insert_id_cart;
+
+  $insertIntoCartItemsQuery = "INSERT INTO cartItems(cartId, productId, price) VALUES
+  ($insert_id_cart,$productId,$price)";
+  $insertIntoCartItemsResult = mysqli_query($link, $insertIntoCartItemsQuery);
+  $insert_id_cartItems = $link->insert_id;
+  // return $insert_id_cartItems;
+
+  $cart_items_query = "SELECT ci.cartItemId, p.productId, p.name, p.price, p.image, p.shortDescription
+                        FROM products AS p
+                        JOIN cartItems AS ci
+                         ON p.productId = ci.productId
+                        WHERE ci.cartItemId = $insert_id_cartItems";
+
+   $cart_items_result = mysqli_query($link, $cart_items_query);
+    // return  mysqli_fetch_assoc($cart_items_result);
+    return [
+      "cart_id" => $insert_id_cart,
+      "cart_items" => mysqli_fetch_assoc($cart_items_result)
+    ];
 }
