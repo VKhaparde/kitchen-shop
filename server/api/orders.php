a34d1cb2efa5b6e5ed2cc9b5f15848d1799a9194
Vandana Khaparde <vandana.khaparde@gmail.com>
2019-11-30 18:55:42 +0000

    Not yet succesful in placing the order getting response body null - Vandana Khaparde

--- server/api/orders.php
+++ server/api/orders.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+// session_start();
 if ($request['method'] === 'POST') {
   if (!$_SESSION['cart_id']) {
     throw new ApiError('Active Shopping Cart is Required', 400);
@@ -8,27 +8,30 @@ if ($request['method'] === 'POST') {
       throw new ApiError('Customer name /creditCard /shippingAddress might be missing', 400);
     }
     $link = get_db_link();
-    $message = place_order($link);
+    $cartId = $_SESSION['cart_id'];
+    $name = "James Cobbler";
+    $creditCard = "12345678";
+    $shippingAddress = "Irvine,CA";
+    $message = place_order($link, $cartId, $name, $creditCard, $shippingAddress);
     $response['body'] = $message['order_placed'];
     send($response);
   }
 }
-function place_order($link)
+function place_order($link,$cartId,$name,$creditCard,$shippingAddress)
 {
-  $cartId = $_SESSION['cart_id'];
-  $name ="";
-  $creditCard = "";
-  $shippingAddress = "";
+
   $sql = "INSERT INTO orders (cartId, name, creditCard, shippingAddress) VALUES (?,?,?,?)";
   $stmt = $link->prepare($sql);
-  $stmt->bind_param("sss", $cartId, $name, $creditCard, $shippingAddress);
-  $result = $stmt->execute();
+  $stmt->bind_param("isss", $cartId, $name, $creditCard, $shippingAddress);
+  $stmt->execute();
   $insert_id = mysqli_stmt_insert_id($stmt);
+  $result = $stmt->get_result();
+  // $row = $result->fetch_assoc();
   $stmt->close();
   unset($_SESSION["cart_id"]);
 
   return [
-    "insert_id "=> $insert_id,
-    "order_placed"=>mysqli_fetch_assoc($result)
+    "insert_id" => $insert_id,
+    "order_placed" => mysqli_fetch_assoc($result)
   ];
 }
