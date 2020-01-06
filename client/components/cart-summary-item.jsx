72d478b04b03017ae82daec54ed873729d301b3f
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-01-06 22:54:40 +0000

    Added container class to cart-summary

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -4,19 +4,21 @@ function CartSummaryItem(props) {
   // console.log('props in CartSummaryItem',props);
   const product = props;
   return (
-    <div className="row col-12 border bg-light m-3 p-3">
-      <div className="col-5">
-        <img src={props.image} alt="productImage"/>
-      </div>
-      <div className="col-7 bg-secondary text-white">
-        <h5 className="font-weight-bold">{props.name}</h5>
-        <p>${props.price}</p>
-        <p>{props.shortDescription}</p>
-        <p>Quantity: {props.count}</p>
-        <button className="btn btn-danger mb-3"
-          onClick={event => {
-            props.removeFromCart(product);
-          } }>Remove</button>
+    <div className="container">
+      <div className="row col-12 border bg-light m-3 p-3">
+        <div className="col-5">
+          <img src={props.image} alt="productImage" />
+        </div>
+        <div className="col-7 bg-secondary text-white">
+          <h5 className="font-weight-bold">{props.name}</h5>
+          <p>${props.price}</p>
+          <p>{props.shortDescription}</p>
+          <p>Quantity: {props.count}</p>
+          <button className="btn btn-danger mb-3"
+            onClick={event => {
+              props.removeFromCart(product);
+            }}>Remove</button>
+        </div>
       </div>
     </div>
   );
