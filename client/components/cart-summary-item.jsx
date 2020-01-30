95b95a8b852d41d168a9678b53ddbb37bb987bb0
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-01-30 05:06:26 +0000

    Remove button removes the first item not the item clicked and minus button delete modal cancel button doesnot work

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -21,7 +21,8 @@ class CartSummaryItem extends React.Component {
     if (this.state.productCount === 0) {
       return (
         <div>
-          <DeleteModal removeItemCompletelyFromCart = {this.props.removeItemCompletelyFromCart}/>
+          <DeleteModal removeItemCompletelyFromCart={this.props.removeItemCompletelyFromCart}
+            product={this.props} />
           <div className="container">
             <div className="row col-12 border bg-light m-3 p-3">
               <div className="col-5 imgStyle">
@@ -45,31 +46,66 @@ class CartSummaryItem extends React.Component {
                     }
                   }}></i></div>
                 </div>
-                <button className="btn btn-danger my-3" data-toggle="modal" data-target="#exampleModal"
+                <button className="btn btn-danger my-3" data-toggle="modal"
                   onClick={event => {
+                    this.showDeleteModal();
                   }}>Remove</button>
               </div>
             </div>
-            <div className="modal fade" tabIndex="-1" id="exampleModal" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-              <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModal"></h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
+
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <div className="row col-12 border bg-light m-3 p-3">
+            <div className="col-5 imgStyle">
+              <img src={this.props.image} alt="productImage" />
+            </div>
+            <div className="col-7 bg-secondary text-white">
+              <h5 className="font-weight-bold mt-4">{this.props.name}</h5>
+              <p>${productPrice}</p>
+              <p>{this.props.shortDescription}</p>
+              <p>Quantity:</p>
+              <div className="d-flex">
+                <div><i className="fas fa-plus-square p-2 mt-2" onClick={event => {
+                  this.props.addToCart(product);
+                }}></i></div>
+                <div className="bg-white text-secondary item_count p-2">{this.props.count}</div>
+                <div><i className="fas fa-minus-square p-2 mt-2" onClick={event => {
+                  if (this.props.count > 1) {
+                    this.props.removeFromCart(product);
+                  } else if (this.props.count === 1) {
+                    this.showDeleteModal();
+                  }
+                }}></i></div>
+              </div>
+              <button className="btn btn-danger my-3" data-toggle="modal" data-target="#exampleModal"
+                onClick={event => {
+                }}>Remove</button>
+            </div>
+          </div>
+          <div className="modal fade" tabIndex="-1" id="exampleModal" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModal"></h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
                   Are you sure you want to remove this item ?
-                  </div>
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" className="btn btn-primary" data-dismiss="modal"
-                      onClick={() => {
-                        this.props.removeItemCompletelyFromCart(product);
-                      }}
-                    >Delete</button>
-                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                  <button type="button" className="btn btn-primary" data-dismiss="modal"
+                    onClick={() => {
+                      // console.log('this.props.productId in cartSummary item', product)
+                      this.props.removeItemCompletelyFromCart(product);
+                    }}
+                  >Delete</button>
                 </div>
               </div>
             </div>
@@ -77,60 +113,6 @@ class CartSummaryItem extends React.Component {
         </div>
       );
     }
-    return (
-      <div className="container">
-        <div className="row col-12 border bg-light m-3 p-3">
-          <div className="col-5 imgStyle">
-            <img src={this.props.image} alt="productImage" />
-          </div>
-          <div className="col-7 bg-secondary text-white">
-            <h5 className="font-weight-bold mt-4">{this.props.name}</h5>
-            <p>${productPrice}</p>
-            <p>{this.props.shortDescription}</p>
-            <p>Quantity:</p>
-            <div className="d-flex">
-              <div><i className="fas fa-plus-square p-2 mt-2" onClick={event => {
-                this.props.addToCart(product);
-              }}></i></div>
-              <div className="bg-white text-secondary item_count p-2">{this.props.count}</div>
-              <div><i className="fas fa-minus-square p-2 mt-2" onClick={event => {
-                if (this.props.count > 1) {
-                  this.props.removeFromCart(product);
-                } else if (this.props.count === 1) {
-                  this.showDeleteModal();
-                }
-              }}></i></div>
-            </div>
-            <button className="btn btn-danger my-3" data-toggle="modal" data-target="#exampleModal"
-              onClick={event => {
-              }}>Remove</button>
-          </div>
-        </div>
-        <div className="modal fade" tabIndex="-1" id= "exampleModal" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModal"></h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-              Are you sure you want to remove this item ?
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" className="btn btn-primary" data-dismiss="modal"
-                  onClick={() => {
-                    this.props.removeItemCompletelyFromCart(product);
-                  }}
-                >Delete</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
   }
 }
 export default CartSummaryItem;
--- client/components/delete-modal.jsx
+++ client/components/delete-modal.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
 function DeleteModal(props) {
+  // console.log('Delete Modal props',props);
   return (
     <div className="container">
-      <div className="modal fade show" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"
+      <div className="modal fade show" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"
         style={{ display: 'block' }}>
         <div className="modal-dialog" role="document">
-          <div className="modal-header">
-            <h3 className="modal-title text-center" id="exampleModalLabel"></h3>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
           <div className="modal-content">
+            <div className="modal-header">
+              <h3 className="modal-title" id="exampleModalLabel"></h3>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
             <div className="modal-body">
-              <p>Are you sure you want to delete this item?</p>
+              <p className="text-center">Are you sure you want to delete this item?</p>
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-              <button type="button" className="btn btn-primary" data-dismiss="modal"
+              <button type="button" className="btn btn-primary"
                 onClick={() => {
-                  // props.removeItemCompletelyFromCart(props);
+                  props.removeItemCompletelyFromCart(props.product);
                 }}>Delete</button>
             </div>
           </div>
